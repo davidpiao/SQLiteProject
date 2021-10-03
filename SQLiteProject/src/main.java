641547fbefba666debb90e801ee35181772b2514
churry97 <63008958+churry97@users.noreply.github.com>
2021-10-03 22:00:07 +0900

    fixed errors

--- SQLiteProject/src/main.java
+++ SQLiteProject/src/main.java
@@ -22,39 +22,39 @@ public class main {
 			}
 			stat1.close();
 			
-//			//추가
-//			System.out.println("\n*** 새 데이터 추가 ***");
-//			Statement stat2 = con.createStatement();
-//			String sql2 = "insert into g_artists (name, a_type, debut, regdate)"
-//			+ " values ('방탄소년단', '남성', '2010년대','2013년',datetime('now','localtime'));";
-//			int cnt = stat2.executeUpdate(sql2);
-//			if (cnt > 0)
-//				System.out.println("새로운 데이터가 추가되었습니다!");
-//			else
-//				System.out.println("데이터추가 오류!");
-//			stat2.close();
-//			
+			//추가
+			System.out.println("\n*** 새 데이터 추가 ***");
+			Statement stat2 = con.createStatement();
+			String sql2 = "insert into g_artists (name, a_type, a_year, debut, regdate)"
+			+ " values ('방탄소년단', '남성', '2010년대','2013년',datetime('now','localtime'));";
+			int cnt = stat2.executeUpdate(sql2);
+			if (cnt > 0)
+				System.out.println("새로운 데이터가 추가되었습니다!");
+			else
+				System.out.println("데이터추가 오류!");
+			stat2.close();
+			
 //			//수정
-//			System.out.println("\n*** 새 데이터 추가 ***");
-//			Statement stat3 = con.createStatement();
-//			String sql3 = "update g_artists set a_year = '2000년대, 2010년대, 2020년대' " + "where id=1;";
-//			int cnt3 = stat3.executeUpdate(sql3);
-//			if (cnt > 0)
-//				System.out.println("새로운 데이터가 수정되었습니다!");
-//			else
-//				System.out.println("데이터 수정 오류!");
-//			stat3.close();
-//			
-//			//삭제
-//			System.out.println("\n*** 새 데이터 삭제 ***");
-//			Statement stat4 = con.createStatement();
-//			String sql4 = "delete from g_artists where id=6 ;";
-//			int cnt4 = stat4.executeUpdate(sql4);
-//			if (cnt4 > 0)
-//				System.out.println("데이타가 삭제되었습니다!");
-//			else
-//				System.out.println("데이터 삭제 오류!");
-//			stat4.close();
+			System.out.println("\n*** 새 데이터 추가 ***");
+			Statement stat3 = con.createStatement();
+			String sql3 = "update g_artists set a_year = '2000년대, 2010년대, 2020년대' " + "where id=1;";
+			int cnt3 = stat3.executeUpdate(sql3);
+			if (cnt > 0)
+				System.out.println("새로운 데이터가 수정되었습니다!");
+			else
+				System.out.println("데이터 수정 오류!");
+			stat3.close();
+			
+			//삭제
+			System.out.println("\n*** 새 데이터 삭제 ***");
+			Statement stat4 = con.createStatement();
+			String sql4 = "delete from g_artists where id=6 ;";
+			int cnt4 = stat4.executeUpdate(sql4);
+			if (cnt4 > 0)
+				System.out.println("데이타가 삭제되었습니다!");
+			else
+				System.out.println("데이터 삭제 오류!");
+			stat4.close();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
